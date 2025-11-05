763f1af6570a739bef3fbfb371453f835604dc43
Andy Atkinson <aoatkinson@gmail.com>
2025-11-05 14:23:24 -0700

    testt commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Elven Docs',
+  tagline: 'Dinosaurs are cooool',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -19,16 +19,16 @@ const config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+url: 'https://aoatkinson.github.io',
+baseUrl: '/elven-docs/', // if deploying as a project page
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+organizationName: 'aoatkinson',
+projectName: 'elven-docs',
+trailingSlash: false,
+
 
   onBrokenLinks: 'throw',
 
